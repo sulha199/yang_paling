db28f937f0158bc39b3cd9b73a288a8458fb193b
Shulha Yahya <surwat@infomedia.co.au>
2019-09-14 11:46:28 +0700

    fix(modify product search layout)

--- src/app/pages/product-search/product-search.component.ts
+++ src/app/pages/product-search/product-search.component.ts
@@ -1,4 +1,8 @@
+import { Select } from '@ngxs/store';
 import { Component, OnInit } from '@angular/core';
+import { ProductSearchState } from 'src/app/core/ngxs/states/productSearch.State';
+import { Observable } from 'rxjs';
+import { ProductSearchStateModel } from 'src/app/core/model/marketplace/productSearch.model';
 
 @Component({
   selector: 'app-product-search',
@@ -6,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-search.component.scss']
 })
 export class ProductSearchComponent implements OnInit {
+  @Select(ProductSearchState) productResults$: Observable<ProductSearchStateModel>;
 
   constructor() { }
 
