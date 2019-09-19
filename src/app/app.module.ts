6bd560ada39f14eab252134bfc12b6c815647617
sulha199 <sulha199@gmail.com>
2019-09-19 07:43:14 +0700

    fix(set Indonesia as locale)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,10 @@ import { IndexComponent } from './pages/index/index.component';
 import { ProductSearchComponent } from './pages/product-search/product-search.component';
 import { ProductSearchState } from './core/ngxs/states/productSearch.State';
 import { RouterModule } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localeId from '@angular/common/locales/id';
+
+registerLocaleData(localeId, 'id');
 
 @NgModule({
   declarations: [
