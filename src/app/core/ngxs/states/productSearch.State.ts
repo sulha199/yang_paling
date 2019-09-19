3d132742c475d11bbcea229f1fdfc983d5fe892d
sulha199 <sulha199@gmail.com>
2019-09-19 07:50:23 +0700

    fix(implement marketplace filter in ngxs)

--- src/app/core/ngxs/states/productSearch.State.ts
+++ src/app/core/ngxs/states/productSearch.State.ts
@@ -1,12 +1,14 @@
 import { MarketplaceService } from './../../services/marketplace/marketplace.service';
 // tslint:disable-next-line:max-line-length
-import { ProductSearchStateModel, DEFAULT_PRODUCT_SEARCH_STATE, ProductSearchParamsModel, ProductSearchResultModel } from 'src/app/core/model/marketplace/productSearch.model';
-import { State, Action, StateContext } from '@ngxs/store';
-import { SearchStart, SearchReset, SearchUpdateSortBy, SearchUpdatePriceRange } from '../actions/productSearch.actions';
+import { ProductSearchStateModel, DEFAULT_PRODUCT_SEARCH_STATE, ProductSearchParamsModel, ProductSearchResultModel, ProductSearchFilter } from 'src/app/core/model/marketplace/productSearch.model';
+import { State, Action, StateContext, Selector } from '@ngxs/store';
+// tslint:disable-next-line:max-line-length
+import { SearchStart, SearchReset, SearchUpdateSortBy, SearchUpdatePriceRange, SearchUpdateFilterMarketPlace } from '../actions/productSearch.actions';
 import { forkJoin, Observable, pipe } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { routesList } from 'src/app/app-routing.module';
+import { MarketPlaceModel } from '../../model/marketplace';
 
 @State<ProductSearchStateModel>({
   name: 'productSearch',
@@ -18,9 +20,28 @@ export class ProductSearchState {
     private router: Router
   ) {}
 
+  @Selector()
+  static marketplaces(state: ProductSearchStateModel): MarketPlaceModel[] {
+    return state.marketplace;
+  }
+
+  ngxsOnInit(ctx: StateContext<ProductSearchStateModel>) {
+    const marketplaceWithProductSearch = this.marketplaceService.members.filter(marketplace => !!marketplace.productSearch);
+    const state: ProductSearchStateModel = {
+      ...DEFAULT_PRODUCT_SEARCH_STATE,
+      marketplace: marketplaceWithProductSearch,
+      filter: {
+        marketplaces: marketplaceWithProductSearch.reduce((obj, marketplace) => {
+          obj[marketplace.basicInfo.name] = true;
+          return obj;
+        }, {})
+      }
+    };
+    ctx.setState(state);
+  }
+
   @Action(SearchStart)
   start(ctx: StateContext<ProductSearchStateModel>, action: SearchStart) {
-    ctx.dispatch(new SearchReset());
     const state  = {
       ...ctx.getState(),
       searchValue: { ...DEFAULT_PRODUCT_SEARCH_STATE.searchValue, ...action.searchValue}
@@ -33,14 +54,19 @@ export class ProductSearchState {
     ).subscribe(all => {
       ctx.setState({
         ...state,
-        results: all
+        results: all,
+        showedResults: this.filterResults(state.filter, all)
       });
     });
   }
 
   @Action(SearchReset)
   reset(ctx: StateContext<ProductSearchStateModel>, action: SearchReset) {
-    ctx.setState(DEFAULT_PRODUCT_SEARCH_STATE);
+    ctx.setState({
+      ...ctx.getState(),
+      results: [],
+      showedResults: [],
+    });
   }
 
   @Action(SearchUpdateSortBy)
@@ -55,15 +81,27 @@ export class ProductSearchState {
     ctx.dispatch(new SearchStart(searchValue));
   }
 
-  // @Action()
+  @Action(SearchUpdateFilterMarketPlace)
+  updateFilterMarketPlace(ctx: StateContext<ProductSearchStateModel>, action: SearchUpdateFilterMarketPlace) {
+    const state = ctx.getState();
+    const filter: ProductSearchFilter = { ...state.filter, marketplaces: action.records };
+    ctx.setState({
+      ...state,
+      filter,
+      showedResults: this.filterResults(filter, state.results)
+    });
+  }
+
   performSearch(state: ProductSearchStateModel): Observable<ProductSearchResultModel[][]> {
-    const results$ = this.marketplaceService.members
-      .filter(marketplace => !!marketplace.productSearch)
-      .map(
-        marketplace => marketplace.productSearch.productSearch(state.searchValue).pipe(
-          map(results => results.map(result => ({ ...result, origin: marketplace.name} as ProductSearchResultModel)))
-        )
-      );
+    const results$ = state.marketplace.map(
+      marketplace => marketplace.productSearch.productSearch(state.searchValue).pipe(
+        map(results => results.map(result => ({ ...result, origin: marketplace.basicInfo.name} as ProductSearchResultModel)))
+      )
+    );
     return forkJoin(results$);
+  }
+
+  filterResults(filter: ProductSearchFilter, rows: ProductSearchResultModel[]) {
+    return rows.filter(row => filter.marketplaces[row.origin]);
   }
 }
--- src/app/pages/product-search/product-search.component.ts
+++ src/app/pages/product-search/product-search.component.ts
@@ -1,8 +1,10 @@
-import { Select } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { Component, OnInit } from '@angular/core';
 import { ProductSearchState } from 'src/app/core/ngxs/states/productSearch.State';
 import { Observable } from 'rxjs';
 import { ProductSearchStateModel } from 'src/app/core/model/marketplace/productSearch.model';
+import { SearchUpdateFilterMarketPlace } from 'src/app/core/ngxs/actions/productSearch.actions';
+import { MarketPlaceModel } from 'src/app/core/model/marketplace';
 
 @Component({
   selector: 'app-product-search',
@@ -12,9 +14,19 @@ import { ProductSearchStateModel } from 'src/app/core/model/marketplace/productS
 export class ProductSearchComponent implements OnInit {
   @Select(ProductSearchState) productResults$: Observable<ProductSearchStateModel>;
 
-  constructor() { }
+  constructor(
+    private store: Store
+  ) { }
 
   ngOnInit() {
+  }
+
+  updateMarketPlaceFilter(value: Record<string, boolean>) {
+    this.store.dispatch(new SearchUpdateFilterMarketPlace(value));
+  }
+
+  searchingStatusCallback(marketplace: MarketPlaceModel) {
+    return marketplace.productSearch.isProcessing$.value;
   }
 
 }
