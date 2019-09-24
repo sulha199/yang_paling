9980ac810d752607d1ba4e67a1a1e60809697680
sulha199 <sulha199@gmail.com>
2019-09-24 08:04:47 +0700

    fix(update product search page)

--- src/app/pages/product-search/product-search.component.ts
+++ src/app/pages/product-search/product-search.component.ts
@@ -1,10 +1,12 @@
 import { Select, Store } from '@ngxs/store';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductSearchState } from 'src/app/core/ngxs/states/productSearch.State';
 import { Observable } from 'rxjs';
 import { ProductSearchStateModel } from 'src/app/core/model/marketplace/productSearch.model';
 import { SearchUpdateFilterMarketPlace } from 'src/app/core/ngxs/actions/productSearch.actions';
 import { MarketPlaceModel } from 'src/app/core/model/marketplace';
+import { MarketplaceService } from 'src/app/core/services/marketplace/marketplace.service';
+import { MarketplaceFilterComponent } from 'src/app/shared/components/marketplace-filter/marketplace-filter.component';
 
 @Component({
   selector: 'app-product-search',
@@ -14,9 +16,17 @@ import { MarketPlaceModel } from 'src/app/core/model/marketplace';
 export class ProductSearchComponent implements OnInit {
   @Select(ProductSearchState) productResults$: Observable<ProductSearchStateModel>;
 
+  @ViewChild('marketplaceFilter', { static: true}) marketplaceFilter: MarketplaceFilterComponent;
+
+  members: MarketPlaceModel[];
+  searchingStatusCallback = (marketplace: MarketPlaceModel) => marketplace.productSearch.isProcessing$;
+
   constructor(
-    private store: Store
-  ) { }
+    private store: Store,
+    private marketplaceService: MarketplaceService
+  ) {
+    this.members = this.marketplaceService.productSearchMembers;
+  }
 
   ngOnInit() {
   }
@@ -24,9 +34,4 @@ export class ProductSearchComponent implements OnInit {
   updateMarketPlaceFilter(value: Record<string, boolean>) {
     this.store.dispatch(new SearchUpdateFilterMarketPlace(value));
   }
-
-  searchingStatusCallback(marketplace: MarketPlaceModel) {
-    return marketplace.productSearch.isProcessing$.value;
-  }
-
 }
