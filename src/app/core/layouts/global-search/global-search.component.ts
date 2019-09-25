9f05e95cb108218d0c24a6cc913e2dcb1b9abe5d
Shulha Yahya <surwat@infomedia.co.au>
2019-09-25 16:31:47 +0700

    fix(switch display when search is fired)

--- src/app/core/layouts/global-search/global-search.component.ts
+++ src/app/core/layouts/global-search/global-search.component.ts
@@ -1,9 +1,11 @@
+import { DISPLAY_MODE_NORMAL } from 'src/app/core/model/displayMode.model';
 import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { MatSelect } from '@angular/material';
 import { Store } from '@ngxs/store';
 import { SearchStart } from 'src/app/core/ngxs/actions/productSearch.actions';
 import { SortBy } from 'src/app/core/model/marketplace/productSearch.model';
+import { SwitchDisplayMode } from '../../ngxs/actions/displayMode.actions';
 
 @Component({
   selector: 'app-global-search',
@@ -48,6 +50,7 @@ export class GlobalSearchComponent implements OnInit {
         text: this.searchValue,
         sortBy: this.sortValue,
       }));
+      this.store.dispatch(new SwitchDisplayMode(DISPLAY_MODE_NORMAL));
     }
   }
 
