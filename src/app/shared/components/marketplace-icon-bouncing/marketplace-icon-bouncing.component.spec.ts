56fb7a04dae548bedae7a3345c931d938bd5ddcb
sulha199 <sulha199@gmail.com>
2019-10-05 14:30:30 +0700

    fix(update bouncing icon)

--- src/app/shared/components/marketplace-icon-bouncing/marketplace-icon-bouncing.component.spec.ts
+++ src/app/shared/components/marketplace-icon-bouncing/marketplace-icon-bouncing.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MarketplaceIconBouncingComponent } from './marketplace-icon-bouncing.component';
+import { MaterialModule } from '../../modules/material.module';
+import { BUKALAPAK_INFO, MarketPlaceModel } from 'src/app/core/model/marketplace';
 
 describe('MarketplaceIconBouncingComponent', () => {
   let component: MarketplaceIconBouncingComponent;
   let fixture: ComponentFixture<MarketplaceIconBouncingComponent>;
+  let iconElement: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MarketplaceIconBouncingComponent ]
+      declarations: [ MarketplaceIconBouncingComponent ],
+      imports: [ MaterialModule ]
     })
     .compileComponents();
   }));
@@ -16,10 +20,15 @@ describe('MarketplaceIconBouncingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MarketplaceIconBouncingComponent);
     component = fixture.componentInstance;
+    component.marketplaces = [
+      { basicInfo: BUKALAPAK_INFO } as MarketPlaceModel
+    ];
     fixture.detectChanges();
+    iconElement = fixture.nativeElement.querySelector('.main-icon');
   });
 
-  it('should create', () => {
+  it('should load marketplace', () => {
     expect(component).toBeTruthy();
+    expect(iconElement.getAttribute('alt')).toBe(BUKALAPAK_INFO.name);
   });
 });
--- src/app/shared/components/marketplace-icon-bouncing/marketplace-icon-bouncing.component.ts
+++ src/app/shared/components/marketplace-icon-bouncing/marketplace-icon-bouncing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MarketPlaceModel } from 'src/app/core/model/marketplace';
-import { Observable, of } from 'rxjs';
+import { Observable, of, combineLatest } from 'rxjs';
+import { map, share, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-marketplace-icon-bouncing',
@@ -12,18 +13,23 @@ export class MarketplaceIconBouncingComponent implements OnInit {
   @Input() getProgressCallback: (marketplace: MarketPlaceModel) => Observable<boolean>;
 
   isInProgressStates$: Record<string, Observable<boolean>> = {};
+  isShowDots$: Observable<boolean>;
   constructor() { }
 
   ngOnInit() {
     (this.marketplaces || []).forEach(marketplace => {
       this.isInProgressStates$[marketplace.basicInfo.name] = this.isInProgress(marketplace.basicInfo.name);
     });
+    this.isShowDots$ = combineLatest(Object.values(this.isInProgressStates$)).pipe(
+      map(values => values.some(value => !!value)),
+      shareReplay(2)
+    );
   }
 
   isInProgress(marketplaceName: string) {
     const currentMarketplace = this.marketplaces.find(marketplace => marketplace.basicInfo.name === marketplaceName);
     return (typeof this.getProgressCallback === 'function' && this.getProgressCallback(currentMarketplace))
-      || of (false);
+      || of (true);
   }
 
 }
